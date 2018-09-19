06678d0dabb80eca2cc0bc6bffe5e62491727a2f
Andrew Sherman <andrew.sherman@wagz.com>
2018-09-19 14:54:43 -0400

    system.console

--- Program.cs
+++ Program.cs
@@ -2,7 +2,13 @@ class Program
 {
     static void Main()
     {
-
+        // Prompt user for minutes exercized
+        System.Console.Write("Enter how many minutes you exercized: ");
         
+        string entry = System.Console.ReadLine(); 
+        // Add minutes exercized to total
+        // Display total minutes exercized to the screen
+        // Repeat until the user quits
+
     }
 }
