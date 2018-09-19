fff91bb3afa4ae17c8e216d39f23d888c949fe2f
Andrew Sherman <andrew.sherman@wagz.com>
2018-09-19 15:03:34 -0400

    namespace/using

--- Program.cs
+++ Program.cs
@@ -1,14 +1,19 @@
-class Program
-{
-    static void Main()
-    {
-        // Prompt user for minutes exercized
-        System.Console.Write("Enter how many minutes you exercized: ");
-        
-        string entry = System.Console.ReadLine(); 
-        // Add minutes exercized to total
-        // Display total minutes exercized to the screen
-        // Repeat until the user quits
+using System;
 
+namespace Zazingo.Fitness
+{
+    class Program
+    {
+        static void Main()
+        {
+            // Prompt user for minutes exercized
+            Console.Write("Enter how many minutes you exercized: ");
+            
+            string entry = Console.ReadLine(); 
+            // Add minutes exercized to total
+            // Display total minutes exercized to the screen
+            // Repeat until the user quits
+
+        }
     }
 }
